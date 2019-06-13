a1969199305f27bfde9a3f420448b38c605d0618
Eric P. Katz <ericpkatz@gmail.com>
2019-06-13 19:54:18 -0400

    add puppeteer

--- server.js
+++ server.js
@@ -1,4 +1,13 @@
 const app = require('express')();
+const puppeteer = require('puppeteer');
+
 app.listen(process.env.PORT);
 
-app.get('/', (req, res, next)=> res.send('hello'));
+
+app.get('/', async(req, res, next)=> {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('https://stackoverflow.com');
+  const content = await page.content();
+  res.send(content);
+});
